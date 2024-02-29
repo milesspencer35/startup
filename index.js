f88e41873db8dd6cf15334ca9ad66958aca2a9e5
Miles Spencer <milesspencer35@gmail.com>
2024-02-29 12:18:52 -0700

    register is saving users correctly

--- index.js
+++ index.js
@@ -12,25 +12,40 @@ function openRegisterPopup() {
     popup.classList.add('open-popup');
 }
 
+let existingUsersText = localStorage.getItem('users');
+let existingUsers = JSON.parse(existingUsersText);
+
 function closeRegisterPopup(type){
     if (type == "submit") {
         const registerUsername = document.querySelector("#RegisterUsername");
         const registerEmail = document.querySelector("#RegisterEmail");
         const registerPassword = document.querySelector("#RegisterPassword");
-        if (!registerUsername.value || !registerEmail.value || !registerPassword.value) {
+
+        if (!registerUsername.value || !registerEmail.value || !registerPassword.value) { //one filled isn't filled out
             
             var message = document.querySelector("#badInfoMessage");
             message.textContent = "Please enter a valid username, email, and password";
+        } else if (existingUsers && existingUsers.some(item =>  item.username === registerUsername.value)) { //username taken
+            var message = document.querySelector("#badInfoMessage");
+            message.textContent = "That username is already taken, please choose a different one.";
         } else {
-
-            // How should I store the users in LocalStorage??
-
+            this.saveUser({username: registerUsername.value, email: registerEmail.value, password:registerPassword.value});
+            localStorage.setItem("currentUsername", registerUsername.value);
             window.location.href = "counter.html";
         }
-    } else {
+    } else { //cancel clicked
         var message = document.querySelector("#badInfoMessage");
         message.textContent = "";
         popup.classList.remove('open-popup');
     }
+}
 
+function saveUser(newUser) {
+    let users = [];
+    const usersText = localStorage.getItem('users');
+    if (usersText) {
+        users = JSON.parse(usersText);
+    }
+    users.push(newUser);
+    localStorage.setItem('users', JSON.stringify(users));
 }
