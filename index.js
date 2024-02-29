186e8b2999d2c1a0bc5307411c8f322cc7e27e4f
Miles Spencer <milesspencer35@gmail.com>
2024-02-28 21:44:08 -0700

    Create popup on register click, errors with resizing.

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 
 function login() {
-    const nameEl = document.querySelector("#InputUsername");
-    localStorage.setItem("userName", nameEl.value);
+
+    // Test to see if the username and password are already in system.
+
     window.location.href = "counter.html";
+}
+  
+let popup = document.getElementById('popup');
+
+function openRegisterPopup() {
+    popup.classList.add('open-popup');
+}
+
+function closeRegisterPopup(type){
+    if (type == "submit") {
+        const registerUsername = document.querySelector("#RegisterUsername");
+        const registerEmail = document.querySelector("#RegisterEmail");
+        const registerPassword = document.querySelector("#RegisterPassword");
+        if (!registerUsername.value || !registerEmail.value || !registerPassword.value) {
+            
+            var message = document.querySelector("#badInfoMessage");
+            message.textContent = "Please enter a valid username, email, and password";
+        } else {
+
+            // How should I store the users in LocalStorage??
+
+            popup.classList.remove('open-popup');
+            window.location.href = "counter.html";
+        }
+    } else {
+        var message = document.querySelector("#badInfoMessage");
+        message.textContent = "";
+        popup.classList.remove('open-popup');
+    }
+
 }
