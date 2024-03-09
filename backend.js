50827e14fcb690a43e9725510ecade5d1d271e5e
Miles Spencer <milesspencer35@gmail.com>
2024-03-08 21:06:52 -0700

    Add foundation code for backend.js

--- backend.js
+++ backend.js
@@ -1,3 +1,28 @@
 const express = require('express');
 const app = express();
 
+// The service port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// User Services
+
+
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+  });
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
