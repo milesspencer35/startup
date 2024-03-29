5ffe0b6054e9818f41475c0eec6bc3aaad9f588e
Miles Spencer <milesspencer35@gmail.com>
2024-03-28 20:47:58 -0600

    Remove mock websocket functin

--- public/inventoryList.js
+++ public/inventoryList.js
@@ -16,38 +16,6 @@ function closeRecentlyAddedPopup () {
     recentlyAddedPopup.classList.remove('open-popup');
 }
 
-// function mockWebsocket () {
-//     setInterval(() => {
-//         const recentlyAddedListEl = document.querySelector('#recentlyAddedList');
-        
-//         recentlyAddedListEl.innerHTML = `
-//         <li class="list-group-item">
-//             <div class="list-group-item-detail">
-//                 <span class="list-group-item-type">Josh</span>
-//                 <span>added &nbsp; &mdash;</span>
-//             </div>
-//             <div class="list-group-item-detail">
-//                 <span class="list-group-item-type">Name: </span>
-//                 <span>BYU Jacket</span>
-//             </div>
-//             <div class="list-group-item-detail">
-//                 <span class="list-group-item-type">UPC: </span>
-//                 <span>123</span>
-//             </div>
-//             <div class="list-group-item-detail">
-//                 <span class="list-group-item-type">Style: </span>
-//                 <span>ABCD-321</span>
-//             </div>
-//             <div class="list-group-item-detail">
-//                 <span class="list-group-item-type">Size: </span>
-//                 <span>M</span>
-//             </div>
-//         </li>` + recentlyAddedListEl.innerHTML;
-//       }, 10000);
-// }
-
-// mockWebsocket();
-
 async function loadRecentlyAdded(items) {
     if (!items) {
         items = await getItems();
